9b05e95364b33a6d2033e39144e85902cf8405d3
Justin McBride <jmcbride@mersive.com>
2019-03-19 17:25:25 -0600

    adding command line stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const program = require('commander');
 const jimp = require('jimp');
 
 const COLOR_BLACK = jimp.rgbaToInt( 0, 0, 0, 255 );
@@ -7,32 +8,30 @@ const COLOR_RED = jimp.rgbaToInt( 255, 0 , 0, 255 );
 const COLOR_GREEN = jimp.rgbaToInt( 0, 255, 0, 255 );
 const COLOR_BLUE = jimp.rgbaToInt( 0, 0 , 255, 255 );
 
-const IMAGE_WIDTH = 1000;
-const IMAGE_HEIGHT = 1000;
-const OUTPUT_FILENAME = "output/output.png";
 
 const BACKGROUND_COLOR = COLOR_BLACK;
 const FOREGROUND_COLOR = COLOR_GREEN;
 const BASE_COLOR = COLOR_RED;
 
-const BOX_SIZE = 50;
+const BOX_SIZE = 1;
+
+
 
 async function main()
 {
+    const IMAGE_WIDTH = program.width;
+    const IMAGE_HEIGHT = program.height;
+    const OUTPUT_FILENAME = `output/checkers_${BOX_SIZE}px_${IMAGE_WIDTH}x${IMAGE_HEIGHT}.png`;
     try
     {        
         const newImage = await new jimp( IMAGE_WIDTH, IMAGE_HEIGHT, BASE_COLOR );
 
-        const numberBoxesWide = IMAGE_WIDTH / BOX_SIZE;
-        for ( let w = 0; w < numberBoxesWide; w += 1 )
+        for ( let w = 0; w < ( IMAGE_WIDTH / BOX_SIZE ); w++ )
         {
-            const numberBoxesHigh = IMAGE_HEIGHT / BOX_SIZE;
-            for ( let h = 0; h < numberBoxesHigh; h += 1 )
+            for ( let h = 0; h < ( IMAGE_HEIGHT / BOX_SIZE ); h++ )
             {
-                let CURRENT_COLOR = BASE_COLOR;
-            
+                let CURRENT_COLOR = BACKGROUND_COLOR;
                 if ( ( w + h ) % 2 ) CURRENT_COLOR = FOREGROUND_COLOR;
-                else CURRENT_COLOR = BACKGROUND_COLOR; 
 
                 for ( let i = 0; i < BOX_SIZE; i++ )
                 {
@@ -51,5 +50,11 @@ async function main()
         console.error( err );
     }
 }
+
+program
+  .version('0.1.0')
+  .option('-w <n>', 'Output width', parseInt )
+  .option('-h <n>', 'Output height', parseInt )
+  .parse(process.argv);
 
 main();
